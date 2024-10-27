fc696fed7b16b21c4c00377c1a5db699212bed23
iKitzu <kennethsantiagoc@gmail.com>
2024-10-27 08:52:50 -0500

    fix: :art: Barra de busqueda implementada, falta la funcionaldiad xd

--- assets/js/script.js
+++ assets/js/script.js
@@ -561,18 +561,15 @@ updateView();
 // ---------------------------------------------------------------------------------------------------------------------------------------------------
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Función para actualizar referencias de elementos dinámicamente
     const getElements = () => ({
         searchButton: document.querySelector('button[aria-label="Buscar"]'),
         closeSearchButton: document.getElementById('closeSearch'),
-        clearButton: document.getElementById('clearSearch'),
         searchView: document.getElementById('searchView'),
         searchInput: document.getElementById('searchInput')
     });
 
-    // Evento de click para manejar la apertura y cierre de la barra de búsqueda
     document.addEventListener('click', (event) => {
-        const { searchButton, closeSearchButton, searchView, searchInput, clearButton } = getElements();
+        const { searchButton, closeSearchButton, searchView, searchInput } = getElements();
         
         if (event.target.closest('button[aria-label="Buscar"]')) {
             searchView.style.display = 'flex';
@@ -581,38 +578,15 @@ document.addEventListener('DOMContentLoaded', () => {
         if (event.target.closest('#closeSearch')) {
             searchView.style.display = 'none';
             searchInput.value = '';
-            clearButton.style.display = 'none';
         }
     });
 
-    // Evento input para mostrar o esconder el botón de limpiar según el contenido del input
-    document.addEventListener('input', (event) => {
-        const { searchInput, clearButton } = getElements();
-        
-        if (event.target === searchInput) {
-            clearButton.style.display = searchInput.value ? 'block' : 'none';
-        }
-    });
-
-    // Evento de click para limpiar el input
-    document.addEventListener('click', (event) => {
-        const { searchInput, clearButton } = getElements();
-        
-        if (event.target === clearButton) {
-            searchInput.value = '';
-            clearButton.style.display = 'none';
-            searchInput.focus();
-        }
-    });
-
-    // Evento para cerrar la barra de búsqueda con la tecla Escape
     document.addEventListener('keydown', (e) => {
-        const { searchView, searchInput, clearButton } = getElements();
+        const { searchView, searchInput } = getElements();
         
         if (e.key === 'Escape' && searchView.style.display === 'flex') {
             searchView.style.display = 'none';
             searchInput.value = '';
-            clearButton.style.display = 'none';
         }
     });
 });
